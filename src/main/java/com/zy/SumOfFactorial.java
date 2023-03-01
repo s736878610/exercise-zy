5b20896e81aea5590073b69e55d10f4d2145120b
zhongyuan <zhongyuan@kfang.com>
2023-03-01 19:54:04 +0800

    update SumOfFactorial

---
+++ src/main/java/com/zy/SumOfFactorial.java
@@ -0,0 +1,48 @@
+package com.zy;
+
+/**
+ * SumOfFactorial
+ * <p>
+ * 1!+2!+3!+4!...+N!
+ *
+ * @author zhongyuan
+ * @since 2023/3/1
+ */
+public class SumOfFactorial {
+
+    public static long f1(int n) {
+        long ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans += factorial(i);
+        }
+        return ans;
+    }
+
+    /**
+     * 求n的阶乘
+     */
+    public static long factorial(int n) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans *= i;
+        }
+        return ans;
+    }
+
+    public static long f2(int n) {
+        long temp = 1;
+        long ans = 0;
+        for (int i = 1; i <= n; i++) {
+            temp *= i;
+            ans += temp;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(f1(n));
+        System.out.println(f2(n));
+    }
+
+}
